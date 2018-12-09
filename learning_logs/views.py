889db0cb5ea74c8700f11432091f1b18ca0f8225
Avi <acohen41@students.towson.edu>
2018-12-09 01:10:58 -0500

    m12

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -17,8 +17,8 @@ def index(request):
 @login_required	
 def topics(request):
 	# Show all topics
-	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
-	context = {'topics': topics}
+	topics = Topic.objects.filter(owner=request.user).order_by("date_added")
+	context = {"topics": topics}
 	return render(request, 'learning_logs/topics.html', context)
 
 @login_required	
